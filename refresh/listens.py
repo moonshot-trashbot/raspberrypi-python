0abe845e2b609ebb28bc93be922a03d6c3884e56
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-16 17:13:43 -0700

    hi

--- refresh/listens.py
+++ refresh/listens.py
@@ -1,27 +1,36 @@
 import socket
-import run
+import json
 
-host = "0.0.0.0"
+global host
+global port
+global sock
+
+host = "127.0.0.1"
 port = 420
-
 sock = socket.socket()
+cont = True
 
 def open():
     global sock
     sock.bind((host, port))
     sock.listen()
 
-def accept():
-    global sock
-    con, addr = sock.accept()
-    while con:
-        data = con.recv(1024)
-        if not data: break
-        strn = str(data.decode('utf-8'))
-        if(strn == ""): strn = "{}\n"
-        strn = strn.split("\\n")[0]
-        run.addition(strn)
+def accept(callback):
+    while cont:
+        con, addr = sock.accept()
+        print(">>> SOCKET: Receiving input from", addr, "... please wait.")
+        chunks = []
+        streaming = True
+        while streaming:
+            chunkjoin = str("".join(chunks))
+            data = con.recv(2)
+            if not data:
+                streaming = False
+                if(chunkjoin != ""):
+                    callback(chunkjoin)
+                    chunks = []
+            strng = str(data.decode('utf-8'))
+            chunks.append(strng)
 
 def close():
-    global sock
-    sock.close()
+    return
--- refresh/run.py
+++ refresh/run.py
@@ -13,17 +13,25 @@
 # PRE-CONFIGURATION
 # ----------------------------------------
 
-import os
-import time
+import traceback
+import multiprocessing
+import signal
+import sys
 import json
+import time
 import _models
 import manager
 import listens
 from threading import Thread
 
+global run
+global stopb
+global recent
+
 run = True
 stopb = False
 save = []
+recent = int(time.time())
 
 
 # ----------------------------------------
@@ -33,28 +41,52 @@ save = []
 # REACCESS() - Get Next Processable Entry
 def reaccess():
     global save
-    x = save.pop()
-    return x
+    global recent
+    global run
+    if(save.__len__() > 0):
+        x = save.pop(0)
+        recent = int(time.time())
+        return x
+    else:
+        final = 300
+        sec = int(time.time()) - recent
+        if(sec <= (final - 180)):
+            print(">>> PROCESSES: THERE IS NO QUEUE LEFT, IN", (str(300 - sec) + "s"),  "I WILL TURN OFF. [Checking every: 5s]")
+            time.sleep(5)
+        elif(sec <= (final - 120)):
+            print(">>> PROCESSES: THERE IS NO QUEUE LEFT, IN", (str(300 - sec) + "s"),  "I WILL TURN OFF. [Checking every 10s]")
+            time.sleep(10)
+        elif(sec < (final - 60)):
+            print(">>> PROCESSES: THERE IS NO QUEUE LEFT, IN", (str(300 - sec) + "s"),  "I WILL TURN OFF. [Checking every 20s]")
+            time.sleep(20)
+        else:
+            if(sec >= final):
+                run = False
+                print(">>> NOTICE: Finished processing, inactive for over 5 minutes, shutting down.")
+                quit()
+                return
 
+# ADDITIOn(Input) - Add Item to Process Queue
 def addition(inp):
+    if(inp == "" or inp == "{}"): return Exception("No input data.")
     global save
     jso = json.loads(inp)
     new = _models.Detection(jso)
     save.append(new)
+    return
 
-# PROCESS(Object) - Run Single Instruction
+# PROCESS(Input) - Run Single Instruction
 def process(inp):
-    print(inp)
+    print(">>> PROCESSES: Starting processing of following object...")
+    print("\t", inp)
+    time.sleep(5)
+    print(">>> PROCESSES: (Fake: Finished Processing, moving on.)")
     pass
 
-# STOP() - Close Connections, Clean-up
-def stop():
-    global stopb
-    if(stopb is False):
-        stopb = True
-        print("Now forcing the program to close down... (check error log if not manually triggered.)")
-        manager.close()
-        listens.close()
+# QUIT() - Request to Close Connections, Clean-up
+def quit():
+    stop(False)
+    sys.exit(130)
 
 
 # ----------------------------------------
@@ -62,29 +94,50 @@ def stop():
 # ----------------------------------------
 
 def coreRobot():
-    manager.open()
-    global run
-    while run:
-        try:
-            x = reaccess()
-            if(x is not None and x is not {}):
-                # (?) Add more here, maybe.
-                process(x)
-        except:
-            run = False
-            stop()
-    stop()
+    while True:
+        x = reaccess()
+        if(x is not None and x is not {}):
+            # (?) Add more here, maybe.
+            process(x)
 
 def coreSocket():
-    listens.open()
-    global run
-    while run:
-        try:
-            listens.accept()
-        except:
-            run = False
-            stop()
-    stop()
+    listens.accept(addition)
 
-Thread(target = coreRobot).start()
-Thread(target = coreSocket).start()
+t1 = Thread(target = coreRobot)
+t2 = Thread(target = coreSocket)
+
+# STOP(Error) - Must Close Connections, Clean-up
+def stop(error):
+    global stopb
+    global run
+    if(stopb is False):
+        stopb = True
+        run = False
+        if(error is False):
+            print(">>> TRACEBACK: Manually requested the program to close after sucessfull runtime. Ignore any following errors.")
+        else:
+            print(">>> TRACEBACK: Now forcing the program to close down... (check error log?)")
+            print(error)
+            traceback.print_tb(error.__traceback__, 5)
+        manager.close()
+        listens.close()
+        sys.exit(0)
+    return None
+
+if __name__ == '__main__':
+    try:
+        manager.open()
+        listens.open()
+    finally:
+        try:
+            t1.start()
+            t2.start()
+        except KeyboardInterrupt as e:
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
+            stop(False)
+            # t2.terminate()
+            # t1.terminate()
+        except Exception as e:
+            stop(e)
+            # t2.terminate()
+            # t1.terminate()
