2852de7094020fa9d85fbd4302195e75438692ac
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-19 16:12:30 -0700

    hi

--- refresh/listens.py
+++ refresh/listens.py
@@ -7,7 +7,7 @@ sock = context.socket(zmq.PULL)
 sock.bind("tcp://*:420")
 cont = True
 
-def accept(callback):
+def accept():
     message = sock.recv().decode("utf-8")
     print(">>> SOCKET: Receiving input from... please wait.")
     print("RAW MSG", message)
