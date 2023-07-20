d338564be790873540dc51424ade241e272b1340
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-20 16:30:32 -0700

    hi

--- refresh/debugs.py
+++ refresh/debugs.py
@@ -64,11 +64,8 @@ def main():
         pygame.draw.rect(screen, pygame.Color(0, 255, 0), rectstripe)
 
         xgen = xMid
-        if(head <= 90 or head >= 180):
-            if(head >= 0 and head <= 90):
-                xgen = xMid + ((head)*4)
-            if(head >= 180 and head >= 259):
-                xgen = xMid - ((360-head)*4)
+        if(head <= 90 and head >= -90):
+            xgen = xMid + ((head)*4)
         vec = [int(xgen), int(yMid-10)]
         pygame.draw.circle(screen, pygame.Color(150, 150, 0), vec, 20)
 
