7a0895ab0481b43c8f09acdc01500cd63a19bd61
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-20 20:58:05 -0700

    hi

--- new/debugs.py
+++ new/debugs.py
@@ -32,7 +32,7 @@ def stripechange(x, y):
     global stripeCenterY
     x = x+60 * camChange
     y = y * camChange
-    shifty = (45*camChange)
+    shifty = (30*camChange)
     stripeCenterX = (screenWidth-x)+shifty
     stripeCenterY = y+shifty
 
