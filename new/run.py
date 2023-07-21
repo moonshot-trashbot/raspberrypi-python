423c29241c6c835024df9ee2c4a46f8b925a72ee
Annabella Proctor <annabellaproctor@gmail.com>
2023-07-20 19:38:40 -0700

    hi

--- new/run.py
+++ new/run.py
@@ -35,56 +35,60 @@ context = zmq.Context()
 sock = context.socket(zmq.PULL)
 sock.bind("tcp://*:420")
 
-async def main():
-    cont = True
-    async def stop(error):
-        global cont
-        cont = False
-        if(error is False):
-            print(">>> TRACEBACK: Manually requested the program to close after sucessfull runtime. Ignore any following errors! This shutdown should take a few seconds.")
-        else:
-            print(">>> TRACEBACK: Now forcing the program to close down... (check error log?) This shutdown should take a few seconds.")
-            print(error)
-            traceback.print_tb(error.__traceback__, 10)
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
-            exit()
-    try:
-        loop = asyncio.get_event_loop()
-        rvr = SpheroRvrAsync(
-            dal=SerialAsyncDal(
-                loop
-            )
-        )
-
-        await rvr.wake()
-        await asyncio.sleep(2)
-        await rvr.led_control.set_all_leds_color(color = Colors.pink)
-
-        tracking = {
-            "id": None,
-            "seen": 0,
-            "center": [None, None]
-        }
-
-        while cont:
-            message = sock.recv().decode("utf-8")
-            print(">>> SOCKET: Receiving input from... please wait.")
+def runner():
+    async def main(loop):
+        cont = True
+        async def stop(error):
+            global cont
             cont = False
-            return message
-        
+            if(error is False):
+                print(">>> TRACEBACK: Manually requested the program to close after sucessfull runtime. Ignore any following errors! This shutdown should take a few seconds.")
+            else:
+                print(">>> TRACEBACK: Now forcing the program to close down... (check error log?) This shutdown should take a few seconds.")
+                print(error)
+                traceback.print_tb(error.__traceback__, 10)
+            try:
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
+                exit()
+        try:
+            rvr = SpheroRvrAsync(
+                dal=SerialAsyncDal(
+                    loop
+                )
+            )
 
-        sock.term()
-        await rvr.close()
-    except KeyboardInterrupt as e:
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-        await stop(False)
-    except Exception as e:
-        signal.signal(signal.SIGINT, signal.SIG_IGN)
-        await stop(e)
+            await rvr.wake()
+            await asyncio.sleep(2)
+            await rvr.led_control.set_all_leds_color(color = Colors.pink)
 
-thr = _classes.StoppableThread(target=main)
+            tracking = {
+                "id": None,
+                "seen": 0,
+                "center": [None, None]
+            }
+
+            while cont:
+                message = sock.recv().decode("utf-8")
+                print(">>> SOCKET: Receiving input from... please wait.")
+                cont = False
+                return message
+            
+
+            sock.term()
+            await rvr.close()
+        except KeyboardInterrupt as e:
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
+            await stop(False)
+        except Exception as e:
+            signal.signal(signal.SIGINT, signal.SIG_IGN)
+            await stop(e)
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
+
+thr = _classes.StoppableThread(target=runner)
 thr.start()
 thr.join()
